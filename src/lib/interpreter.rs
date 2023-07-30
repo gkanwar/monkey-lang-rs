7125b6cc7157465ac11c22728f5030e372a66dc2
Gurtej Kanwar <gurtej@mit.edu>
2023-07-30 14:47:18 +0100

    interpreter: autofmt

--- src/lib/interpreter.rs
+++ src/lib/interpreter.rs
@@ -1,8 +1,7 @@
 use crate::parser;
-use std::{rc::Rc, collections::HashMap, collections::hash_map::Entry};
+use std::{collections::hash_map::Entry, collections::HashMap, rc::Rc};
 
-
-#[derive(Clone,Debug,PartialEq,Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Value {
   Int(i64),
   Bool(bool),
@@ -52,7 +51,9 @@ impl State {
     Self { scopes: vec![] }
   }
   fn push_scope(&mut self) {
-    self.scopes.push(Scope { bindings: HashMap::new() });
+    self.scopes.push(Scope {
+      bindings: HashMap::new(),
+    });
   }
   fn pop_scope(&mut self) {
     self.scopes.pop();
@@ -76,10 +77,11 @@ pub enum RuntimeError {
 fn expect_bool_value(value: &Value) -> Result<bool, RuntimeError> {
   if let Value::Bool(b) = value {
     Ok(*b)
-  }
-  else {
+  } else {
     Err(RuntimeError::TypeError(std::format!(
-          "expected bool, received {:?}", value)))
+      "expected bool, received {:?}",
+      value
+    )))
   }
 }
 
@@ -87,20 +89,22 @@ fn expect_bool_value(value: &Value) -> Result<bool, RuntimeError> {
 fn expect_number_value(value: &Value) -> Result<i64, RuntimeError> {
   if let Value::Int(i) = value {
     Ok(*i)
-  }
-  else {
+  } else {
     Err(RuntimeError::TypeError(std::format!(
-          "expected int, received {:?}", value)))
+      "expected int, received {:?}",
+      value
+    )))
   }
 }
 
 fn expect_string_value(value: &Value) -> Result<String, RuntimeError> {
   if let Value::Str(s) = value {
     Ok(s.clone()) // TODO
-  }
-  else {
+  } else {
     Err(RuntimeError::TypeError(std::format!(
-          "expected string, received {:?}", value)))
+      "expected string, received {:?}",
+      value
+    )))
   }
 }
 
@@ -110,9 +114,9 @@ fn eval_expression(
   idents: &Vec<String>,
   strings: &Vec<String>,
 ) -> Result<Value, RuntimeError> {
-  use parser::Expr;
   use parser::Atomic::*;
   use parser::BinaryOp::*;
+  use parser::Expr;
   let value = match expr {
     Expr::BinaryOp(bin_op, left, right) => {
       let value_l = eval_expression(left, state, idents, strings)?;
@@ -128,28 +132,28 @@ fn eval_expression(
           let bool_r = expect_bool_value(&value_r)?;
           Value::Bool(bool_l || bool_r)
         }
-        CmpEquals => {
-          match value_l {
-            Value::Int(i) => Value::Bool(i == expect_number_value(&value_r)?),
-            Value::Bool(b) => Value::Bool(b == expect_bool_value(&value_r)?),
-            Value::Str(s) => Value::Bool(s == expect_string_value(&value_r)?),
-            _ => {
-              return Err(RuntimeError::TypeError(
-                std::format!("value {:?} does not support equality check", value_l)));
-            }
+        CmpEquals => match value_l {
+          Value::Int(i) => Value::Bool(i == expect_number_value(&value_r)?),
+          Value::Bool(b) => Value::Bool(b == expect_bool_value(&value_r)?),
+          Value::Str(s) => Value::Bool(s == expect_string_value(&value_r)?),
+          _ => {
+            return Err(RuntimeError::TypeError(std::format!(
+              "value {:?} does not support equality check",
+              value_l
+            )));
           }
-        }
-        CmpNotEquals => {
-          match value_l {
-            Value::Int(i) => Value::Bool(i != expect_number_value(&value_r)?),
-            Value::Bool(b) => Value::Bool(b != expect_bool_value(&value_r)?),
-            Value::Str(s) => Value::Bool(s != expect_string_value(&value_r)?),
-            _ => {
-              return Err(RuntimeError::TypeError(
-                std::format!("value {:?} does not support equality check", value_l)));
-            }
+        },
+        CmpNotEquals => match value_l {
+          Value::Int(i) => Value::Bool(i != expect_number_value(&value_r)?),
+          Value::Bool(b) => Value::Bool(b != expect_bool_value(&value_r)?),
+          Value::Str(s) => Value::Bool(s != expect_string_value(&value_r)?),
+          _ => {
+            return Err(RuntimeError::TypeError(std::format!(
+              "value {:?} does not support equality check",
+              value_l
+            )));
           }
-        }
+        },
         CmpLess => {
           let num_l = expect_number_value(&value_l)?;
           let num_r = expect_number_value(&value_r)?;
@@ -205,7 +209,7 @@ fn eval_expression(
         None => {
           return Err(RuntimeError::NameError(idents[*ident].clone()));
         }
-      }
+      },
       _ => {
         todo!();
       }
@@ -238,8 +242,7 @@ fn eval_statement(
           } else {
             if let Some(no_stmts) = no {
               eval_statements(no_stmts, state, idents, strings)?
-            }
-            else {
+            } else {
               None
             }
           };
@@ -258,9 +261,7 @@ fn eval_statement(
       state.pop_scope();
       value
     }
-    Bare(expr) => {
-      Some(eval_expression(expr, state, idents, strings)?)
-    }
+    Bare(expr) => Some(eval_expression(expr, state, idents, strings)?),
   };
   Ok(value)
 }
@@ -281,8 +282,7 @@ fn eval_statements(
 pub fn interpret(prog: parser::Program) -> Result<Option<Value>, RuntimeError> {
   let mut state = State::new();
   state.push_scope(); // global scope
-  let out = eval_statements(
-    &prog.statements, &mut state, &prog.idents, &prog.strings);
+  let out = eval_statements(&prog.statements, &mut state, &prog.idents, &prog.strings);
   state.pop_scope();
   out
 }
